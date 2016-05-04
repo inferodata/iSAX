7f843120f4e1ff7acad0a3389f52b17ead8661e8
Voices from the Blogs Srl <blogsvoices@unimi.it>
2016-05-04 14:32:44 +0200

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,13 @@
-#.First.lib <- function(lib, pkg) library.dynam("sde", pkg, lib) 
-
-#.noGenerics <- TRUE
+#########################################################################################################
+# iSAX is an R package which provides access to iSA technology developed by 
+# VOICES from the Blogs. It is released for academic use only and licensed 
+# under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License 
+# see http://creativecommons.org/licenses/by-nc-nd/4.0/
+# Warning: Commercial use of iSA is protected under the U.S. provisional patent application No. 62/215264
+#########################################################################################################
 
 .onAttach <- function(libname, pkgname)
 {
-    # require(methods)
- 
-    # require(zoo)
  packageStartupMessage("\n")
  packageStartupMessage(rep("#",63))
  packageStartupMessage("# iSA: U.S. provisional patent application No. 62/215264      #")
@@ -15,7 +16,5 @@
  packageStartupMessage("# For academic use only!                                      #")
  packageStartupMessage(rep("#",63))
  packageStartupMessage("\n")
-# require(KernSmooth, quietly=TRUE)
-# library.dynam("yuima", pkgname, libname) 
 }
 
