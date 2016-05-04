bf86c83351ff50d3643927770a3fa44c4f76ca2d
Voices from the Blogs Srl <blogsvoices@unimi.it>
2016-05-04 14:31:35 +0200

    added (c) notes

--- R/iSA.R
+++ R/iSA.R
@@ -1,3 +1,11 @@
+#########################################################################################################
+# iSAX is an R package which provides access to iSA technology developed by 
+# VOICES from the Blogs. It is released for academic use only and licensed 
+# under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License 
+# see http://creativecommons.org/licenses/by-nc-nd/4.0/
+# Warning: Commercial use of iSA is protected under the U.S. provisional patent application No. 62/215264
+#########################################################################################################
+
 iSA <-
 function(Strain, Stest, Dtrain, nboot=1000, predict=FALSE, ret.boot=FALSE, seqlen=5, sparse=FALSE, verbose=TRUE){
     ptm <- proc.time()
